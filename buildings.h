604569eff1a4a51f9c2a941f3ad57d705261c2a8
yunus662 <imyaay234@gmail.com>
2025-06-23 12:26:49 -0700

    Create buildings.h

---
+++ buildings.h
@@ -0,0 +1,10 @@
+#ifndef BUILDINGS_H
+#define BUILDINGS_H
+
+// Buildings module header
+// Declares functions for initializing, updating, and cleaning up the buildings module.
+bool initBuildings();
+void updateBuildings();
+void cleanupBuildings();
+
+#endif // BUILDINGS_H
